3ced6492943c257b66653b3877ec1ab4dc3f93c3
ARecabarren <arecabarren14@alumnos.utalca.cl>
2022-01-03 23:20:16 +0100

    Create while_exercice_1.py

---
+++ MITx-6.00.1x/Week_1-_Python_Basics/while_exercice_1.py
@@ -0,0 +1,16 @@
+#1. Convert the following into code that uses a while loop.
+
+#prints 2
+#prints 4
+#prints 6
+#prints 8
+#prints 10
+#prints Goodbye!
+
+
+
+i = 2
+while i < 11:
+        print(i)
+        i = i+2
+print('Goodbye!')
