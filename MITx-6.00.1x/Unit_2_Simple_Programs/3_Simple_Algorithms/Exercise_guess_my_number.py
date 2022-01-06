9788f1d2662a122caa6c165efee7f45261347839
ARecabarren <arecabarren14@alumnos.utalca.cl>
2022-01-06 12:11:59 +0100

    Exercise: guess my number

--- MITx-6.00.1x/Unit_2_Simple_Programs/3_Simple_Algorithms/Exercise_guess_my_number.py
+++ MITx-6.00.1x/Unit_2_Simple_Programs/3_Simple_Algorithms/Exercise_guess_my_number.py
@@ -1,6 +1,8 @@
 # In this problem, you'll create a program that guesses a secret number!
 #
-# The program works as follows: you (the user) thinks of an integer between 0 (inclusive) and 100 (not inclusive). The computer makes guesses, and you give it input - is its guess too high or too low? Using bisection search, the computer will guess the user's secret number!
+# The program works as follows: you (the user) thinks of an integer between 0 (inclusive) and 100 (not inclusive).
+# The computer makes guesses, and you give it input - is its guess too high or too low? Using bisection search, the computer will
+# guess the user's secret number!
 #
 # Here is a transcript of an example session:
 #
@@ -30,3 +32,32 @@
 # Sorry, I did not understand your input.
 # Is your secret number 91?
 # Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. c
+
+def bisectionSearch(min,max):
+    return (min + max)//2
+
+def getAnswer(center):
+    while True:
+        answer = input('Is your secret number ' + str(center) + '?' + '\n' +
+        'Enter "h" to indicate the guess is too high. Enter "l" to indicate the guess is too low. Enter "c" to indicate I guessed correctly. ')
+        if answer in 'hlc':
+            return answer
+        else:
+            print('Sorry, I did not understand your input.')
+
+
+def startGame():
+    print('Please think of a number between 0 and 100!')
+    lower = 0
+    high = 100
+    guess = bisectionSearch(lower,high)
+    answer = getAnswer(guess)
+    while answer != 'c':
+        if answer == 'h':
+            high = guess
+        else:
+            lower = guess
+        guess = bisectionSearch(lower,high)
+        answer = getAnswer(guess)
+
+startGame()
